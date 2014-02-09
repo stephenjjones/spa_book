a5ec33b882943eeea90bd1bd7c252aa9893dc531
vagrant <vagrant@devweb>
2014-02-09 20:36:55 +0000

    section 4.2 complete

---
+++ spa/js/spa.model.js
@@ -0,0 +1,8 @@
+/*jslint
+
+* /
+/*global $, spa */
+
+spa.model = (function () {
+    return {};
+}());
--- spa/js/spa.shell.js
+++ spa/js/spa.shell.js
@@ -181,6 +181,9 @@ spa.shell = (function () {
             schema_map : configMap.anchor_schema_map
         });
 
+        spa.chat.configModule( {} );
+        spa.chat.initModule ( jqueryMap.$chat );
+
         $(window)
             .bind( 'hashchange', onHashchange )
             .trigger( 'hashchange' );
